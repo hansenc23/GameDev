f31964f421fd0f511a88d5fc51f15063c9c7888f
Hansen Christian <13052434@student.uts.edu.au>
2019-08-31 16:26:22 +1000

    add falling

---
+++ Assets/Scripts/Tetromino.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetromino : MonoBehaviour
+{
+    float fall = 0;
+    public float fallSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - fall >= fallSpeed)
+        {
+            transform.position += new Vector3(0, -1, 0);
+            fall = Time.time;
+        }
+    }
+}
