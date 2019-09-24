e747fdc5dfca866fec82cb35d8035c911bea137e
Hansen Christian <13052434@student.uts.edu.au>
2019-09-24 17:12:02 +1000

    add user input,rotation,boundaries

--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Tetromino : MonoBehaviour
 {
+    public Vector3 rotationPoint;
     float fall = 0;
     public float fallSpeed = 1;
+    public static int height = 20;
+    public static int width = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +18,49 @@ public class Tetromino : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Time.time - fall >= fallSpeed)
+        if(Time.time - fall >= (Input.GetKey(KeyCode.DownArrow) ? fallSpeed/10 : fallSpeed))
         {
             transform.position += new Vector3(0, -1, 0);
+            if (!ValidMove())
+                transform.position -= new Vector3(0, -1, 0);
             fall = Time.time;
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            if(!ValidMove())
+                transform.position -= new Vector3(-1, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if (!ValidMove())
+                transform.position -= new Vector3(1, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+            if (!ValidMove())
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+        }
+    }
+
+    bool ValidMove()
+    {
+        foreach(Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if(roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
