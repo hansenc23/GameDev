ddf624d7d7b6714b049465e2214eab94d0809078
Hansen Christian <13052434@student.uts.edu.au>
2019-10-02 13:03:46 +1000

    add rotation, boundaries, sound effects, line clear

--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -9,9 +9,20 @@ public class Tetromino : MonoBehaviour
     public float fallSpeed = 1;
     public static int height = 20;
     public static int width = 10;
+    private static Transform[,] grid = new Transform[width, height];
+    public Animator[] glowAnim;
+    public AudioClip[] clips = new AudioClip[2];
+    
+
     // Start is called before the first frame update
+    private void Awake()
+    {
+        
+    }
     void Start()
     {
+        glowAnim = gameObject.GetComponentsInChildren<Animator>();
+        
         
     }
 
@@ -22,9 +33,18 @@ public class Tetromino : MonoBehaviour
         {
             transform.position += new Vector3(0, -1, 0);
             if (!ValidMove())
+            {
                 transform.position -= new Vector3(0, -1, 0);
+                updateGrid();
+                FindObjectOfType<Spawner>().Spawn();
+                this.enabled = false;
+                
+            }
+                
             fall = Time.time;
         }
+
+        
 
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
@@ -42,10 +62,30 @@ public class Tetromino : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
-            if (!ValidMove())
-                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+            if (!gameObject.CompareTag("O"))
+            {
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+                gameObject.GetComponent<AudioSource>().clip = clips[0];
+                gameObject.GetComponent<AudioSource>().Play();
+                if (!ValidMove())
+                    transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+            }
+           
         }
+
+        if (this.enabled == false)
+        {
+            Debug.Log("DONE");
+            //this.GetComponentInChildren<Animator>().enabled =
+
+            foreach(Animator children in glowAnim)
+            {
+                //children.GetComponent<Animator>().enabled = false;
+                Destroy(children.GetComponent<Animator>());
+            }
+        }
+
+        CheckLines();
     }
 
     bool ValidMove()
@@ -59,8 +99,79 @@ public class Tetromino : MonoBehaviour
             {
                 return false;
             }
+
+            if(grid[roundedX, roundedY] != null)
+            {
+                return false;
+            }
         }
 
         return true;
+    }
+
+    void updateGrid()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            grid[roundedX, roundedY] = children;         
+        }
+
+        
+    }
+
+    void CheckLines()
+    {
+       
+        for (int i = height - 1; i >= 0; i--)
+        {
+            if (HasLine(i))
+            {
+                gameObject.GetComponent<AudioSource>().clip = clips[1];
+                gameObject.GetComponent<AudioSource>().Play();
+                DeleteLine(i);
+                RowDown(i);
+            }
+        }
+    }
+
+    bool HasLine(int i)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            if(grid[j,i] == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void DeleteLine(int i)
+    {
+        for(int j = 0; j < width; j++)
+        {
+           
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+        }
+    }
+
+    void RowDown(int i)
+    {
+        for(int k = i; k < height; k++)
+        {
+            for(int j = 0; j< width; j++)
+            {
+                if(grid[j,k] != null)
+                {
+                    grid[j, k - 1] = grid[j, k];
+                    grid[j, k] = null;
+                    grid[j, k - 1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
+        }
     }
 }
