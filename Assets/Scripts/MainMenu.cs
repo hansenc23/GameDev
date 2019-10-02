79cd6f4c9b85d3f1975527f56cc0bc988fa24fc7
Hansen Christian <13052434@student.uts.edu.au>
2019-10-02 22:59:49 +1000

    add main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button[] buttons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void audio()
+    {
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+
+    public void loadGame()
+    {
+        SceneManager.LoadScene(1);
+        DontDestroyOnLoad(this.gameObject);
+    }
+        
+}
